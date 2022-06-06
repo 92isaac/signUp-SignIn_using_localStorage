46cff75ad64cb75464eb53847c8725f867c6999f
Sangodare Isaac <sangodareisaac92@gmail.com>
2022-06-06 16:50:50 +0100

    add home page and the script file

---
+++ script.js
@@ -0,0 +1,103 @@
+const signUp = e => {
+    let fname = document.getElementById('fname').value,
+        lname = document.getElementById('lname').value,
+        email = document.getElementById('email').value,
+        pwd = document.getElementById('pwd').value;
+
+    let formData = JSON.parse(localStorage.getItem('formData')) || [];
+
+    let exist = formData.length && 
+        JSON.parse(localStorage.getItem('formData')).some(data => 
+            data.fname.toLowerCase() == fname.toLowerCase() && 
+            data.lname.toLowerCase() == lname.toLowerCase()
+        );
+
+    if(!exist){
+        formData.push({ fname, lname, email, pwd });
+        localStorage.setItem('formData', JSON.stringify(formData));
+        document.querySelector('form').reset();
+        document.getElementById('fname').focus();
+        alert("Account Created.\n\nPlease Sign In using the link below.");
+    }
+    else{
+        alert("Ooopppssss... Duplicate found!!!\nYou have already signed up");
+    }
+    e.preventDefault();
+}
+
+function signIn(e) {
+    let email = document.getElementById('email').value, pwd = document.getElementById('pwd').value;
+    let formData = JSON.parse(localStorage.getItem('formData')) || [];
+    let exist = formData.length && 
+    JSON.parse(localStorage.getItem('formData')).some(data => data.email.toLowerCase() == email && data.pwd.toLowerCase() == pwd);
+    if(!exist){
+        alert("Incorrect login credentials");
+    }
+    else{
+        window.location.href = "./home.html";
+    }
+    e.preventDefault();
+}
+
+
+
+
+
+
+// const signUp =(e)=>{
+
+    
+// }
+
+// document.querySelector("#form").addEventListener("submit", ()=>{
+//     let fname = document.getElementById("fname").value,
+//     lname = document.getElementById("lname").value,
+//     email = document.getElementById("email").value,
+//     pwd = document.getElementById("pwd").value;
+
+
+//     let formData = JSON.parse(localStorage.getItem('formData')) || [];
+
+//     let exist = formData.length && 
+//     JSON.parse(localStorage.getItem('formData')).some(data => 
+//         data.fname.toLowerCase() == fname.toLowerCase() && 
+//         data.lname.toLowerCase() == lname.toLowerCase()
+//     );
+
+//     if(!exist){
+//         formData.push({ fname, lname, email, pwd });
+//         localStorage.setItem('formData', JSON.stringify(formData));
+//         document.querySelector('form').reset();
+//         document.getElementById('fname').focus();
+//         alert("Account Created.\n\nPlease Sign In using the link below.");
+
+//     }
+//     else{
+//         alert("Duplicate form data found");
+//     }
+//     e.preventDefault();
+
+// })
+
+
+
+// const signIn =(e)=>{
+//     let email = document.querySelector(".email").value, 
+//     pwd = document.querySelector(".pwd").value;
+
+//      let formData = JSON.parse(localStorage.getItem('formData')) || [];
+//     let exist = formData.length && 
+//     JSON.parse(localStorage.getItem('formData')).some(data => data.email.toLowerCase() == email && data.pwd.toLowerCase() == pwd);
+
+
+//     if(!exist){
+//         alert("Incorrect details")
+//     }else{
+//         window.location.href= './home.html'
+//     }
+
+// e.preventDefault();
+// }
+
+
+// document.getElementById("signin").addEventListener("click", signIn)
